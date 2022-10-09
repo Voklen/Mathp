d60fa889f1cefd78ce7e7d7da0955b202df6da8b
Alex Gorichev <Alex.Gorichev@protonmail.com>
2022-10-09 10:32:30 +0100

    WORKING!

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,79 @@
 use std::str::Chars;
 
-pub fn parse(string: String) {
-	evaluate_expression(string.chars());
+#[derive(Debug)]
+enum Expression {
+	Func(Function),
+	Num(i64),
 }
 
-fn evaluate_expression(mut iter: Chars) -> Option<Chars> {
-	let character = iter
-		.next()
-		.unwrap_or_else(|| throw("Missing closing bracket"));
+#[derive(Debug)]
+struct Function {
+	name: String,
+	arguments: Vec<Expression>,
+}
+
+pub fn parse(string: String) {
+	let expr = evaluate_expression(&mut string.chars(), None);
+	println!("{:?}", expr)
+}
+
+fn evaluate_expression(iter: &mut Chars, next: Option<char>) -> Expression {
+	let character = match next {
+		Some(i) => i,
+		None => iter.next().unwrap_or_else(throw_eof),
+	};
 	match character {
-		' ' => evaluate_expression(iter),
-		'(' => todo!(),
-		')' => todo!(),
-		_ => todo!(),
+		' ' => evaluate_expression(iter, None),
+		'(' => {
+			let name = get_next_word(iter, None);
+			let arguments = parse_function(iter, None, Vec::<Expression>::new());
+			let func = Function { name, arguments };
+			Expression::Func(func)
+		}
+		i => get_num(iter, Some(i)),
 	}
+}
+
+fn parse_function(
+	iter: &mut Chars,
+	next: Option<char>,
+	mut arguments: Vec<Expression>,
+) -> Vec<Expression> {
+	let character = match next {
+		Some(i) => i,
+		None => iter.next().unwrap_or_else(throw_eof),
+	};
+	if character == ')' {
+		return arguments;
+	}
+	let expr = evaluate_expression(iter, Some(character));
+	arguments.push(expr);
+	match iter.next() {
+		Some(next) => parse_function(iter, Some(next), arguments),
+		None => arguments,
+	}
+}
+
+fn get_num(iter: &mut Chars, next: Option<char>) -> Expression {
+	let num_string = get_next_word(iter, next);
+	Expression::Num(num_string.parse().unwrap())
+}
+
+fn get_next_word(iter: &mut Chars, next: Option<char>) -> String {
+	let mut character = match next {
+		Some(i) => i,
+		None => iter.next().unwrap_or_else(throw_eof),
+	};
+	let mut string = "".to_string();
+	while !(character.is_whitespace() || character == ')') {
+		string.push(character);
+		character = iter.next().unwrap_or_else(throw_eof)
+	}
+	string
+}
+
+pub fn throw_eof() -> char {
+	throw("Unexpected end of file: Missing closing bracket")
 }
 
 pub fn throw(error: &str) -> char {
