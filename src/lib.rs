f3530611ae44db2cc6a7f085aae713d718253e64
Alex Gorichev <Alex.Gorichev@protonmail.com>
2022-10-09 10:55:37 +0100

    Use Peekable iterator

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::str::Chars;
+use std::{iter::Peekable, str::Chars};
 
 #[derive(Debug)]
 enum Expression {
@@ -13,57 +13,52 @@ struct Function {
 }
 
 pub fn parse(string: String) {
-	let expr = evaluate_expression(&mut string.chars(), None);
+	let expr = evaluate_expression(&mut string.chars().peekable());
 	println!("{:?}", expr)
 }
 
-fn evaluate_expression(iter: &mut Chars, next: Option<char>) -> Expression {
-	let character = match next {
-		Some(i) => i,
-		None => iter.next().unwrap_or_else(throw_eof),
-	};
+fn evaluate_expression(iter: &mut Peekable<Chars>) -> Expression {
+	let character = iter.peek().unwrap_or_else(throw_eof);
 	match character {
-		' ' => evaluate_expression(iter, None),
+		' ' => {
+			iter.next();
+			evaluate_expression(iter)
+		},
 		'(' => {
-			let name = get_next_word(iter, None);
-			let arguments = parse_function(iter, None, Vec::<Expression>::new());
+			iter.next();
+			let name = get_next_word(iter);
+			let arguments = parse_function(iter, Vec::<Expression>::new());
 			let func = Function { name, arguments };
 			Expression::Func(func)
 		}
-		i => get_num(iter, Some(i)),
+		_ => get_num(iter),
 	}
 }
 
 fn parse_function(
-	iter: &mut Chars,
-	next: Option<char>,
+	iter: &mut Peekable<Chars>,
 	mut arguments: Vec<Expression>,
 ) -> Vec<Expression> {
-	let character = match next {
-		Some(i) => i,
-		None => iter.next().unwrap_or_else(throw_eof),
-	};
-	if character == ')' {
+	let character = iter.peek().unwrap_or_else(throw_eof);
+	if character == &')' {
+		iter.next();
 		return arguments;
 	}
-	let expr = evaluate_expression(iter, Some(character));
+	let expr = evaluate_expression(iter);
 	arguments.push(expr);
-	match iter.next() {
-		Some(next) => parse_function(iter, Some(next), arguments),
+	match iter.peek() {
+		Some(_) => parse_function(iter, arguments),
 		None => arguments,
 	}
 }
 
-fn get_num(iter: &mut Chars, next: Option<char>) -> Expression {
-	let num_string = get_next_word(iter, next);
+fn get_num(iter: &mut Peekable<Chars>) -> Expression {
+	let num_string = get_next_word(iter);
 	Expression::Num(num_string.parse().unwrap())
 }
 
-fn get_next_word(iter: &mut Chars, next: Option<char>) -> String {
-	let mut character = match next {
-		Some(i) => i,
-		None => iter.next().unwrap_or_else(throw_eof),
-	};
+fn get_next_word(iter: &mut Peekable<Chars>) -> String {
+	let mut character = iter.next().unwrap_or_else(throw_eof);
 	let mut string = "".to_string();
 	while !(character.is_whitespace() || character == ')') {
 		string.push(character);
