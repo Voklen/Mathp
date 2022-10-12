bd09fddbe262d057e663b2e0225fa0ee4840c905
Alex Gorichev <Alex.Gorichev@protonmail.com>
2022-10-12 08:36:02 +0100

    Clean up HashMap

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -19,11 +19,11 @@ fn evaluate_function(func: Function) -> i64 {
 			Expression::Num(num) => num,
 		}
 	}
-	let mut functions = HashMap::<&str, &dyn Fn(Vec<i64>) -> i64>::new();
-	functions.insert("+", &add);
-	functions.insert("-", &sub);
-	functions.insert("*", &mul);
-	functions.insert("/", &div);
+	let mut functions = HashMap::<&str, MathsFunction>::new();
+	functions.insert("+", add);
+	functions.insert("-", sub);
+	functions.insert("*", mul);
+	functions.insert("/", div);
 	let new_func = match functions.get(&func.name as &str) {
 		Some(i) => i,
 		None => throw("No function {func.name}"),
@@ -31,6 +31,8 @@ fn evaluate_function(func: Function) -> i64 {
 	let new_args = convert_args(func.arguments);
 	new_func(new_args)
 }
+
+type MathsFunction = fn(Vec<i64>) -> i64;
 
 fn add(x: Vec<i64>) -> i64 {
 	x.into_iter().fold(0, |total, new| total + new)
